9b728596071a0b12f6d0d4397b893c1d78dc5a51
Rodrigo <rodrigocf_92@hotmail.com>
2018-07-15 12:33:34 -0600

    changed from query to stored procedure

--- functions/1-restart_game.sql
+++ functions/1-restart_game.sql
@@ -1,20 +1,26 @@
-update WAR_MASTER.REINOS
-set ORO        = 40000,
-    MADERA     = 20000,
-    HIERRO     = 10000,
-    CORONAS    = 0,
-    DEFENSA    = 0,
-    ATAQUE     = 0,
-    ARQUEROS   = 0,
-    PIQUEROS   = 0,
-    CABALLEROS = 0,
-    MAGOS      = 0;
+create or replace procedure WAR_MASTER.restart_game as
 
-update WAR_MASTER.RESERVA_CENTRAL
-set ORO           = 5000000,
-    MADERA        = 2000000,
-    HIERRO        = 2000000,
-    PRECIO_MADERA = 2.000,
-    PRECIO_HIERRO = 4.000;
+    begin
 
-truncate table WAR_MASTER.TRANSACCIONES;
+        update WAR_MASTER.REINOS
+        set ORO        = 40000,
+            MADERA     = 20000,
+            HIERRO     = 10000,
+            CORONAS    = 0,
+            DEFENSA    = 0,
+            ATAQUE     = 0,
+            ARQUEROS   = 0,
+            PIQUEROS   = 0,
+            CABALLEROS = 0,
+            MAGOS      = 0;
+
+        update WAR_MASTER.RESERVA_CENTRAL
+        set ORO           = 5000000,
+            MADERA        = 2000000,
+            HIERRO        = 2000000,
+            PRECIO_MADERA = 2.000,
+            PRECIO_HIERRO = 4.000;
+
+        execute immediate 'truncate table WAR_MASTER.TRANSACCIONES';
+
+    end;
