a8b0449f274b2508401906a33950f3be8ed5657b
Rodrigo <rodrigocf_92@hotmail.com>
2018-08-18 11:22:51 -0600

    added ability to detect when not theres not enough money or resources to complete buy or sell

--- stored_procedures/2-buy.sql
+++ stored_procedures/2-buy.sql
@@ -2,8 +2,14 @@ create or replace procedure WAR_MASTER.buy(p_material varchar2, p_amount_bought 
 
     p_reserve_material number;
     p_material_price   number(10, 3);
+    p_kingdom_gold     number;
 
     begin
+
+        select GOLD
+        into p_kingdom_gold
+        from WAR_MASTER.KINGDOMS
+        where ID_KINGDOM = p_kingdom;
 
         if p_amount_bought > 0
         then
@@ -17,24 +23,31 @@ create or replace procedure WAR_MASTER.buy(p_material varchar2, p_amount_bought 
                     into p_reserve_material, p_material_price
                     from WAR_MASTER.CENTRAL_RESERVE;
 
-                    update WAR_MASTER.KINGDOMS
-                    set WOOD   = WOOD + p_amount_bought,
-                        gold   = gold - (p_material_price * p_amount_bought),
-                        CROWNS = CROWNS + 5
-                    where ID_KINGDOM = p_kingdom;
+                    if p_kingdom_gold < (p_material_price * p_amount_bought)
+                    then
+                        update WAR_MASTER.KINGDOMS
+                        set WOOD   = WOOD + p_amount_bought,
+                            gold   = gold - (p_material_price * p_amount_bought),
+                            CROWNS = CROWNS + 5
+                        where ID_KINGDOM = p_kingdom;
 
-                    update WAR_MASTER.CENTRAL_RESERVE
-                    set GOLD = GOLD + (p_material_price * p_amount_bought),
-                        WOOD = WOOD - p_amount_bought;
+                        update WAR_MASTER.CENTRAL_RESERVE
+                        set GOLD = GOLD + (p_material_price * p_amount_bought),
+                            WOOD = WOOD - p_amount_bought;
 
-                    p_material_price :=
-                    p_material_price + (p_material_price * round(p_amount_bought / p_reserve_material, 2));
+                        p_material_price :=
+                        p_material_price + (p_material_price * round(p_amount_bought / p_reserve_material, 2));
 
-                    update WAR_MASTER.CENTRAL_RESERVE
-                    set WOOD_PRICE = p_material_price;
+                        update WAR_MASTER.CENTRAL_RESERVE
+                        set WOOD_PRICE = p_material_price;
 
-                    insert into WAR_MASTER.TRANSACTIONS (TRANSACTION_ID, TRANSACTION_TYPE, ID_KINGDOM, WOOD, CROWNS)
-                    values (WAR_MASTER.TRANSAC_SEQ.nextval, 'CMP', p_kingdom, p_amount_bought, 5);
+                        insert into WAR_MASTER.TRANSACTIONS (TRANSACTION_ID, TRANSACTION_TYPE, ID_KINGDOM, WOOD, CROWNS)
+                        values (WAR_MASTER.TRANSAC_SEQ.nextval, 'CMP', p_kingdom, p_amount_bought, 5);
+
+                    else
+                        dbms_output.put_line('No tiene suficiente oro para completar la transaccion.');
+                    end if;
+
 
                 when 'HIERRO'
                 then
@@ -44,24 +57,30 @@ create or replace procedure WAR_MASTER.buy(p_material varchar2, p_amount_bought 
                     into p_reserve_material, p_material_price
                     from WAR_MASTER.CENTRAL_RESERVE;
 
-                    update WAR_MASTER.KINGDOMS
-                    set IRON   = IRON + p_amount_bought,
-                        gold   = gold - (p_material_price * p_amount_bought),
-                        CROWNS = CROWNS + 5
-                    where ID_KINGDOM = p_kingdom;
+                    if p_kingdom_gold < (p_material_price * p_amount_bought)
+                    then
+                        update WAR_MASTER.KINGDOMS
+                        set IRON   = IRON + p_amount_bought,
+                            gold   = gold - (p_material_price * p_amount_bought),
+                            CROWNS = CROWNS + 5
+                        where ID_KINGDOM = p_kingdom;
 
-                    update WAR_MASTER.CENTRAL_RESERVE
-                    set GOLD = GOLD + (p_material_price * p_amount_bought),
-                        IRON = IRON - p_amount_bought;
+                        update WAR_MASTER.CENTRAL_RESERVE
+                        set GOLD = GOLD + (p_material_price * p_amount_bought),
+                            IRON = IRON - p_amount_bought;
 
-                    p_material_price :=
-                    p_material_price + (p_material_price * round(p_amount_bought / p_reserve_material, 2));
+                        p_material_price :=
+                        p_material_price + (p_material_price * round(p_amount_bought / p_reserve_material, 2));
 
-                    update WAR_MASTER.CENTRAL_RESERVE
-                    set IRON_PRICE = p_material_price;
+                        update WAR_MASTER.CENTRAL_RESERVE
+                        set IRON_PRICE = p_material_price;
 
-                    insert into WAR_MASTER.TRANSACTIONS (TRANSACTION_ID, TRANSACTION_TYPE, ID_KINGDOM, IRON, CROWNS)
-                    values (WAR_MASTER.TRANSAC_SEQ.nextval, 'CMP', p_kingdom, p_amount_bought, 5);
+                        insert into WAR_MASTER.TRANSACTIONS (TRANSACTION_ID, TRANSACTION_TYPE, ID_KINGDOM, IRON, CROWNS)
+                        values (WAR_MASTER.TRANSAC_SEQ.nextval, 'CMP', p_kingdom, p_amount_bought, 5);
+                    else
+                        dbms_output.put_line('No tiene suficiente oro para completar la transaccion.');
+                    end if;
+
 
             end case;
 
--- stored_procedures/3-sell.sql
+++ stored_procedures/3-sell.sql
@@ -2,6 +2,7 @@ create or replace procedure WAR_MASTER.sell(p_material varchar2, p_amount_sold n
 
     p_reserve_material number;
     p_material_price   number(10, 3);
+    p_kingdom_material number;
 
     begin
 
@@ -17,24 +18,35 @@ create or replace procedure WAR_MASTER.sell(p_material varchar2, p_amount_sold n
                     into p_reserve_material, p_material_price
                     from WAR_MASTER.CENTRAL_RESERVE;
 
-                    update WAR_MASTER.KINGDOMS
-                    set WOOD   = WOOD - p_amount_sold,
-                        gold   = gold + (p_material_price * p_amount_sold),
-                        CROWNS = CROWNS + 10
+                    select WOOD
+                    into p_kingdom_material
+                    from WAR_MASTER.KINGDOMS
                     where ID_KINGDOM = p_kingdom;
 
-                    update WAR_MASTER.CENTRAL_RESERVE
-                    set GOLD = GOLD - (p_material_price * p_amount_sold),
-                        WOOD = WOOD + p_amount_sold;
+                    if p_kingdom_material < p_amount_sold
+                    then
+                        update WAR_MASTER.KINGDOMS
+                        set WOOD   = WOOD - p_amount_sold,
+                            gold   = gold + (p_material_price * p_amount_sold),
+                            CROWNS = CROWNS + 10
+                        where ID_KINGDOM = p_kingdom;
 
-                    p_material_price :=
-                    p_material_price - (p_material_price * round(p_amount_sold / p_reserve_material, 2));
+                        update WAR_MASTER.CENTRAL_RESERVE
+                        set GOLD = GOLD - (p_material_price * p_amount_sold),
+                            WOOD = WOOD + p_amount_sold;
 
-                    update WAR_MASTER.CENTRAL_RESERVE
-                    set WOOD_PRICE = p_material_price;
+                        p_material_price :=
+                        p_material_price - (p_material_price * round(p_amount_sold / p_reserve_material, 2));
 
-                    insert into WAR_MASTER.TRANSACTIONS (TRANSACTION_ID, TRANSACTION_TYPE, ID_KINGDOM, WOOD, CROWNS)
-                    values (WAR_MASTER.TRANSAC_SEQ.nextval, 'VTA', p_kingdom, p_amount_sold, 10);
+                        update WAR_MASTER.CENTRAL_RESERVE
+                        set WOOD_PRICE = p_material_price;
+
+                        insert into WAR_MASTER.TRANSACTIONS (TRANSACTION_ID, TRANSACTION_TYPE, ID_KINGDOM, WOOD, CROWNS)
+                        values (WAR_MASTER.TRANSAC_SEQ.nextval, 'VTA', p_kingdom, p_amount_sold, 10);
+                    else
+                        dbms_output.put_line('No tiene suficientes recursos para vender');
+                    end if;
+
 
                 when 'HIERRO'
                 then
@@ -44,24 +56,34 @@ create or replace procedure WAR_MASTER.sell(p_material varchar2, p_amount_sold n
                     into p_reserve_material, p_material_price
                     from WAR_MASTER.CENTRAL_RESERVE;
 
-                    update WAR_MASTER.KINGDOMS
-                    set IRON   = IRON - p_amount_sold,
-                        gold   = gold + (p_material_price * p_amount_sold),
-                        CROWNS = CROWNS + 10
+                    select IRON
+                    into p_kingdom_material
+                    from WAR_MASTER.KINGDOMS
                     where ID_KINGDOM = p_kingdom;
 
-                    update WAR_MASTER.CENTRAL_RESERVE
-                    set GOLD = GOLD - (p_material_price * p_amount_sold),
-                        IRON = IRON + p_amount_sold;
+                    if p_kingdom_material < p_amount_sold
+                    then
+                        update WAR_MASTER.KINGDOMS
+                        set IRON   = IRON - p_amount_sold,
+                            gold   = gold + (p_material_price * p_amount_sold),
+                            CROWNS = CROWNS + 10
+                        where ID_KINGDOM = p_kingdom;
 
-                    p_material_price :=
-                    p_material_price - (p_material_price * round(p_amount_sold / p_reserve_material, 2));
+                        update WAR_MASTER.CENTRAL_RESERVE
+                        set GOLD = GOLD - (p_material_price * p_amount_sold),
+                            IRON = IRON + p_amount_sold;
 
-                    update WAR_MASTER.CENTRAL_RESERVE
-                    set IRON_PRICE = p_material_price;
+                        p_material_price :=
+                        p_material_price - (p_material_price * round(p_amount_sold / p_reserve_material, 2));
 
-                    insert into WAR_MASTER.TRANSACTIONS (TRANSACTION_ID, TRANSACTION_TYPE, ID_KINGDOM, IRON, CROWNS)
-                    values (WAR_MASTER.TRANSAC_SEQ.nextval, 'VTA', p_kingdom, p_amount_sold, 10);
+                        update WAR_MASTER.CENTRAL_RESERVE
+                        set IRON_PRICE = p_material_price;
+
+                        insert into WAR_MASTER.TRANSACTIONS (TRANSACTION_ID, TRANSACTION_TYPE, ID_KINGDOM, IRON, CROWNS)
+                        values (WAR_MASTER.TRANSAC_SEQ.nextval, 'VTA', p_kingdom, p_amount_sold, 10);
+                    else
+                        dbms_output.put_line('No tiene suficientes recursos para vender');
+                    end if;
 
             end case;
 
